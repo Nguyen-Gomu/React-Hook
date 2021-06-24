3ae51cba3abee48ab3e756be06d9ceace926ba43
NguyenTran <dm2019.ttnguyen.3105@gmail.com>
2021-06-24 15:55:02 +0700

    seconds commnit

--- easy-frontend/src/App.js
+++ easy-frontend/src/App.js
@@ -1,32 +1,78 @@
 import React,{useState,useEffect} from 'react';
-import './App.css';
+import queryString from 'query-string'
 import PostLists from './components/PostLists';
+import Pagination from './components/Pagination'
+import PostFilterForm from './components/Search';
+import Clock from './components/Clock';
 
 function App() {
-
+  // States
   const [postList, setPostList] = useState([]);
+  const [pagination,setPagination] = useState({
+    _page:1,
+    _limit:10,
+    _totalRows:1
+  });
+
+  const [filters,setFilters] = useState({
+    _limit:10,
+    _page:1,
+ 
+  })
 
   useEffect(() =>{
     async function fetchPostList() {
       try {
-        const requestUrl = "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+        const paramsString = queryString.stringify(filters);
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
         const response = await fetch(requestUrl);
         const responseJSON = await response.json();
   
         console.log({responseJSON})
-        const {data} = responseJSON;
+        const {data,pagination} = responseJSON;
         setPostList(data);
+        setPagination(pagination);
       } catch (error) {
         console.log("Failed to fetch POST_LIST:",error.message)
       }
     }
+    console.log("nguyen1")
     fetchPostList();
-  },[]);
+  },[filters]);
+
+
+  function handlePageChange(newPage){
+    console.log("new page: ",newPage)
+    setFilters({
+      ...filters,
+      _page:newPage
+    })
+  }
+
+  function handleFilterChange(newFilters){
+    setFilters({
+      ...filters,
+      _gape:1,
+      title_like:newFilters.searchTerm,
+    })
+  }
+
+  const [showClock,setShowClock] = useState(true);
+
+
 
   return (
     <div className="App">
+
      <h1>cd easy-frontend</h1>
-     <PostLists posts={postList}/>
+     {/* <PostLists posts={postList}/>
+     <Pagination
+        pagination={pagination}
+        onPageChange={handlePageChange}
+     />
+     <PostFilterForm onSubmit={handleFilterChange}/> */}
+     <button onClick={() => setShowClock(false)}>Hide Clock</button>
+     {showClock && <Clock/>}
     </div>
   );
 }
